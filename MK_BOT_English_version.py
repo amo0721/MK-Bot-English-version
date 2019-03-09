57fe04ca7e0283fcb428e729a841c0bb3fde240a
amo0721 <46621867+amo0721@users.noreply.github.com>
2019-03-09 20:00:48 +0900

    Update MK_BOT_English_version.py

--- MK_BOT_English_version.py
+++ MK_BOT_English_version.py
@@ -16,44 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-                if "MK notice " in message.content:
-                    if message.author.id == Setting.owner_id:
-                        # DPNK 사용 구문 시점
-                        embed=discord.Embed(title="MK BOT all notice system", color=0xb2ebf4)
-                        embed.add_field(name="Send Notice is readying!", value="요청자 : <@" + message.author.id + ">", inline=True)
-                        mssg = await client.send_message(message.channel, embed=embed)
-                        a = []
-                        b = []
-                        e = []
-                        ec = {}
-                        embed=discord.Embed(title="MK BOT all notice system", color=0xb2ebf4)
-                        embed.add_field(name="I'm send all servers notice!", value="요청자 : <@" + message.author.id + ">", inline=True)
-                        await client.edit_message(mssg, embed=embed)
-                        for server in client.servers:
-                            for channel in server.channels:
-                                for tag in ["notice", "공지", "알림", "Alarm"]:
-                                    if tag in channel.name:
-                                        dtat = True
-                                        for distag in ["밴", "경고", "제재", "길드", "ban", "worry", "warn", "guild"]:
-                                            if distag in channel.name:
-                                                dtat = False
-                                        if dtat:
-                                            if not server.id in a:
-                                                try:
-                                                    await client.send_message(channel, message.content)
-                                                except discord.HTTPException:
-                                                    e.append(str(channel.id))
-                                                    ec[channel.id] = "HTTPException"
-                                                except app.Forbidden:
-                                                    e.append(str(channel.id))
-                                                    ec[channel.id] = "Forbidden"
-                                                except app.NotFound:
-                                                    e.append(str(channel.id))
-                                                    ec[channel.id] = "NotFound"
-                             
-    
-     
-                if "MK finsh working" == message.content:
+                if "MK off" == message.content:
                     if message.author.id == Setting.owner_id:
                         await client.send_message(message.channel, "<@%s>, Stops the bot. Offline within 5 minutes (Discord API Delay)." % (message.author.id))
                         await client.change_presence(game=discord.Game(name="Offline", type=0))
